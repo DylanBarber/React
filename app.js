b3ccefd9ab6beca142dc8b489dded168b6992e23
Dylan Barber <dbarber@Administrators-MacBook-Pro.local>
2019-06-20 08:25:03 -0400

    Learned Composition and React Components

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const title = <h1 class = "header1"> MY HEADING YO </h1>
+function Header(){
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+            
+        </header>
+    );
+};
+function Player(){
+    return (
+        <div className="player">
+            <span className="player-name">
+                Dylan
+            </span>
+            <Counter />
+        </div>
+
+    );
+};
+function Counter(){
+    return (
+        <div className="counter">
+            <button className="counter-add">-</button>
+            <span className="counter-score">35</span>
+            <button className="counter-subtract">+</button>
+        </div>
+    );
+};
 ReactDOM.render(
-    title,
+    <Player />,
     document.getElementById('root')
 );
